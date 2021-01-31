54c2c43a9374fae230f1be4377a4f4dfbffa9d9d
Imane MEKIDECHE <i.mekideche@anem.dz>
2021-01-31 14:19:33 +0100

    Exploring Angular's CSS Encapsulation

--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -19,11 +19,18 @@ import { Component } from '@angular/core';
       <h4> {{thumbnail.someProperty}} </h4> -->
       <!-- <button class="btn btn-primary" (click)="thumbnail.logFoo()" >Log me some foo!</button> -->
 
+      <div class="well">
+        <div>Hello World! 👋</div>
+      </div>
+
       <app-event-thumbnail
         [event]="event1"
       ></app-event-thumbnail>
     </div>
-  `
+  `,
+  styles : [`
+    .well div {color: #bbb;}
+  `]
 })
 
 export class EventsListComponent {
