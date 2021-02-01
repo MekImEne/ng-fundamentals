c5d95965b8d80371d77d6d583709fd660aa57b12
Imane MEKIDECHE <i.mekideche@anem.dz>
2021-02-01 11:34:45 +0100

    Handling Null Values with Safe-Navigation operator

--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -148,6 +148,7 @@ export class EventsListComponent {
         city: 'Amsterdam',
         country: 'Netherlands'
       },
+      onlineUrl : 'https://www.url.com',
       sessions: [
         {
           id: 1,
