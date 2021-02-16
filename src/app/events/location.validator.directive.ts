60227204b9861d3e3b781a960c177578f7af051e
Imane MEKIDECHE <i.mekideche@anem.dz>
2021-02-16 10:34:31 +0100

    Implementing a multi-field validator

--- src/app/events/location.validator.directive.ts
+++ src/app/events/location.validator.directive.ts
@@ -13,6 +13,7 @@ export class LocationValidator implements Validator {
         let cityControl = formGroup.controls['city'];
         let countryControl = formGroup.controls['country'];
         let onlineUrlControl = (<FormGroup>formGroup.root).controls['onlineUrl'];
+        console.log('here');
 
         if ( (addressControl && addressControl.value && cityControl && cityControl.value && countryControl && countryControl.value)
         ||
