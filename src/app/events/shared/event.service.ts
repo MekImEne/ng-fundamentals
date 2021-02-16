1599a73adc27fa0b21a2026f6557bd5ba2e1795f
Imane MEKIDECHE <i.mekideche@anem.dz>
2021-02-16 15:41:46 +0100

    Using QueryString parameters

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -29,28 +29,11 @@ export class EventService {
     ));
   }
 
-  /* updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id);
-    EVENTS[index] = event;
-  } */
 
-  searchSessions(searchTerm: string){
-    var term = searchTerm.toLocaleLowerCase();
-    var results: ISession[] = [];
-    EVENTS.forEach(event => {
-      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      });
-      results = results.concat(matchingSessions);
-    });
-
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]>{
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm).pipe(catchError(
+      this.handleError<ISession[]>('searchSessions')
+    ));
   }
 
   private handleError<T>(operation= 'operation', result?: T) {
