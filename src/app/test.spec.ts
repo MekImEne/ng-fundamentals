8c635106dbc350f27d77f2237b17a465893d5a34
i.mekideche <i.mekideche@anem.dz>
2021-02-17 15:20:17 +0100

    Unit testing - Unit testing services - test delete voters

--- src/app/test.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('first tests', () => {
-    it('Should be true', () => {
-        expect(true).toBe(true);
-    });
-
-    it('Should be false', () => {
-        expect(false).toBe(false);
-    });
-});
