449b8f34f599ba56aac21297b3130ea821ef5a13
i.mekideche <i.mekideche@anem.dz>
2021-02-17 14:57:23 +0100

    Unit testing - Karma

---
+++ src/app/test.spec.ts
@@ -0,0 +1,9 @@
+describe('first tests', () => {
+    it('Should be true', () => {
+        expect(true).toBe(true);
+    });
+
+    it('Should be false', () => {
+        expect(false).toBe(false);
+    });
+});
