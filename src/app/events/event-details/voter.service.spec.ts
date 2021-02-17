b211adddb93b8727e21510ed10905736df851f4c
i.mekideche <i.mekideche@anem.dz>
2021-02-17 15:31:03 +0100

    Unit testing - Unit testing services - test delete voters url and add voter url

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -19,5 +19,23 @@ describe('VoterService', () => {
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe('john');
         });
+
+        it('Should call http.delete with the right URL', () => {
+            var session = {id: 6, voters: ['joe', 'john']};
+            mockHttp.delete.and.returnValue(of(false));
+            voterService.deleteVoter(3, <ISession>session, 'joe');
+
+            expect(mockHttp.delete).toHaveBeenCalledWith(`/api/events/3/sessions/6/voters/joe`);
+        });
+    });
+
+    describe('addVoter', () => {
+        it('Should call http.post with the right URL', () => {
+            var session = {id: 6, voters: ['john']};
+            mockHttp.post.and.returnValue(of(false));
+            voterService.addVoter(3, <ISession>session, 'joe');
+
+            expect(mockHttp.post).toHaveBeenCalledWith(`/api/events/3/sessions/6/voters/joe`, {}, jasmine.any(Object));
+        });
     });
 });
